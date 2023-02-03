ee1c9c4db9fee9ad5e585b76ab0e48bae03d6e1c
Matheusbcy <matheusbcy@gmail.com>
2023-02-03 13:54:27 -0300

    projeto finalizado

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import './Button.css'
+
+function Button (props) {
+  return (
+    <>
+    <button onClick={() => props.click(props.label)} className={`
+      button
+      ${props.operation ? 'operation' : ''}
+      ${props.double ? 'double' : ''}
+      ${props.triple ? 'triple' : ''}
+    `}>
+      {props.label}
+      </button>
+    </>
+  )
+}
+export default Button
